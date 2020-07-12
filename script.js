3cdf3ba732935eb463f36d2d1bde85504d7902b3
CookieStep <62571477+CookieStep@users.noreply.github.com>
2020-07-12 15:57:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let canvas = document.createElement("canvas"), ctx = canvas.getContext("2d");
 let page = document.createElement("canvas"), pen = page.getContext("2d");
 let {assign} = Object,
-	{sin, cos, floor, PI, random, sqrt, pow} = Math;
+	{sin, cos, floor, round, PI, random, sqrt, pow} = Math;
 	game = {
-		width: 500,
-		height: 500
+		width: 250,
+		height: 250
 	}
 onload = () => {
 	let {width, height} = game;
@@ -19,27 +19,58 @@ let update = () => {
 	page.width = innerWidth;
 	page.height = innerHeight;
 	player.update();
-	let imageData = ctx.getImageData(0, 0, 500, 500);
-	for(let i = 3; i < (imageData.data.length); i += 4) {
-		if(imageData.data[i]) imageData.data[i] -= 15;
-		if(imageData.data[i] < 0) imageData.data[i] = 0;
+	{
+		let imageData = ctx.getImageData(0, 0, 500, 500);
+		for(let i = 3; i < (imageData.data.length); i += 4) {
+			if(imageData.data[i]) imageData.data[i] -= 15;
+			if(imageData.data[i] < 0) imageData.data[i] = 0;
+		}
+		while(random() < 99/100) {
+			let a = imageData.data.length/4
+			a = floor(random() * a) * 4;
+			for(let b = 0; b < 4; b++) {
+				imageData.data[a + b] = floor(random() * (b - 3? 256: 64)) + (b - 3? 0: 192);
+			}
+		}
+		ctx.putImageData(imageData, 0, 0);
 	}
-	ctx.putImageData(imageData, 0, 0);
 	player.draw();
 	let lL = innerWidth < innerHeight? innerWidth: innerHeight;
 	for(var _x = -2; _x <= 2; _x++) for(var _y = -2; _y <= 2; _y++) {
-		pen.fillRect(Math.floor((innerWidth - lL)/2 + _x * lL + ((-player.x/game.width + 1/2) * lL)), Math.floor((innerHeight - lL)/2 + _y * lL + ((-player.y/game.height + 1/2) * lL)), lL, lL);
-		pen.drawImage(canvas, Math.floor((innerWidth - lL)/2 + _x * lL + ((-player.x/game.width + 1/2) * lL)), Math.floor((innerHeight - lL)/2 + _y * lL + ((-player.y/game.height + 1/2) * lL)), lL, lL);
+		pen.fillRect(round((innerWidth - lL)/2 + _x * lL + ((-player.x/game.width + 1/2) * lL)), round((innerHeight - lL)/2 + _y * lL + ((-player.y/game.height + 1/2) * lL)), lL, lL);
+		pen.drawImage(canvas, round((innerWidth - lL)/2 + _x * lL + ((-player.x/game.width + 1/2) * lL)), round((innerHeight - lL)/2 + _y * lL + ((-player.y/game.height + 1/2) * lL)), lL, lL);
 	}
 	requestAnimationFrame(update);
 };
-let Player = function() {
-	var velocity = {x: 0, y: 0};
+function Entity() {
+	var velocity = {x: 0, y: 0}
 	assign(this, {
-		velocity, radian: PI * 3/2,
-		x: game.width/2, y: game.height/2,
-		r: 15, rot: 0, friction: 0.8,
+		velocity, radian: 0,
+		x: 0, y: 0,
+		r: 5, rot: 0, friction: 0.8,
 		spd: 0.02,
+		turn(d) {this.rot += d * PI/180},
+		tick() {},
+		update() {
+			this.tick();
+			this.x %= game.width;
+			if(this.x < 0) this.x += game.width;
+			this.y %= game.height;
+			if(this.y < 0) this.y += game.height;
+			this.x += velocity.x;
+			this.y += velocity.y;
+			this.radian += this.rot;
+			this.rot *= this.friction;
+			velocity.x *= sqrt(this.friction);
+			velocity.y *= sqrt(this.friction);}
+	});
+}
+let Player = function() {
+	Entity.call(this);
+	var {velocity} = this;
+	assign(this, {
+		radian: PI * 3/2,
+		x: game.width/2, y: game.height/2,
 		turn(d) {this.rot += d * PI/180},
 		draw() {
 			for(var _x = -1; _x <= 1; _x++) for(var _y = -1; _y <= 1; _y++) {
@@ -79,7 +110,7 @@ let Player = function() {
 				ctx.fill(); ctx.stroke();
 			}
 		},
-		update() {
+		tick() {
 			if(keys.a) this.turn(-1);
 			if(keys.d) this.turn(1);
 			if(keys.w) {
@@ -90,16 +121,6 @@ let Player = function() {
 				velocity.x -= cos(this.radian) * this.r * this.spd;
 				velocity.y -= sin(this.radian) * this.r * this.spd;
 			}
-			this.x %= game.width;
-			if(this.x < 0) this.x += game.width;
-			this.y %= game.height;
-			if(this.y < 0) this.y += game.height;
-			this.x += velocity.x;
-			this.y += velocity.y;
-			this.radian += this.rot;
-			this.rot *= this.friction;
-			velocity.x *= sqrt(this.friction);
-			velocity.y *= sqrt(this.friction);
 		}
 	});
 }
